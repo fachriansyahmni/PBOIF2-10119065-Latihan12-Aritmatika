1a4b2e72c2636b9fe8def6ffb60042d47ebbfad2
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-18 16:48:11 +0700

    commit

---
+++ src/pboif2_10119065_latihan12_aritmatika/PBOIF2_10119065_Latihan12_Aritmatika.java
@@ -0,0 +1,30 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini berisi aritmatika
+ *
+ */
+package pboif2_10119065_latihan12_aritmatika;
+
+public class PBOIF2_10119065_Latihan12_Aritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println("a + b = "+ (a + b));
+        System.out.println("a - b = "+ (a - b));
+        System.out.println("a * b = "+ (a * b));
+        System.out.println("b / a = "+ (b / a));
+        System.out.println("b % a = "+ (b % a));
+        System.out.println("c % a = "+ (c % a));
+        System.out.println("a++ = "+(a++));
+        System.out.println("b-- = "+(b--));
+        System.out.println("d++ = "+(d++));
+        System.out.println("++d = "+(++d));
+    }
+    
+}
